ce2dcd3aac04b5b9db7c2fe75bf5b9000ad17071
AlexC <alexclinton133@gmail.com>
2025-04-21 13:07:47 +0100

    add TCPNetworkLayer class with client/server constructors, connect/send/receive/disconnect methods and internal stream setup.

---
+++ src/network/TCPNetworkLayer.java
@@ -0,0 +1,49 @@
+package network;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class TCPNetworkLayer {
+    private Socket dataSocket;
+    private Scanner scanner;
+    private PrintWriter outputStream;
+    private final String hostname;
+    private final int port;
+
+    // Client-side constructor
+    public TCPNetworkLayer(String hostname, int port) {
+        this.hostname = hostname;
+        this.port = port;
+    }
+
+    // Server-side constructor
+    public TCPNetworkLayer(Socket socket) throws IOException {
+        this.dataSocket = socket;
+        this.hostname = null;
+        this.port = -1;
+        setStreams();
+    }
+
+    private void setStreams() throws IOException {
+        this.scanner = new Scanner(dataSocket.getInputStream());
+        this.outputStream = new PrintWriter(dataSocket.getOutputStream(), true);
+    }
+
+    public void connect() throws IOException {
+        this.dataSocket = new Socket(hostname, port);
+        setStreams();
+    }
+
+    public void send(String message) {
+        outputStream.println(message);
+    }
+
+    public String receive() {
+        return scanner.nextLine();
+    }
+
+    public void disconnect() throws IOException {
+        if (dataSocket != null) dataSocket.close();
+    }
+}
