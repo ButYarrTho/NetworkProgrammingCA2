f6826770a2479db921906c4060adf9c407f63808
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2025-05-03 14:05:30 +0100

    Add subject and body properties to email

--- src/model/Email.java
+++ src/model/Email.java
@@ -1,4 +1,15 @@
 package model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NonNull;
+
+@Data
+@AllArgsConstructor
+@Builder
 public class Email {
+    private String subject;
+    @NonNull
+    private String body;
 }
