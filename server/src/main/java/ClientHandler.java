1c20d3c14f9a8a968e0bbffb31efd56ccda8c6cf
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2025-05-03 20:28:28 +0100

    Add test code to client handler

--- server/src/main/java/ClientHandler.java
+++ server/src/main/java/ClientHandler.java
@@ -1,14 +1,25 @@
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class ClientHandler implements Runnable {
-    private final TCPNetworkLayer networkLayer;
+    private final TCPNetworkLayer network;
     private final Storage storage;
 
     public ClientHandler(TCPNetworkLayer networkLayer, Storage storage) {
-        this.networkLayer = networkLayer;
+        this.network = networkLayer;
         this.storage = storage;
     }
 
     @Override
     public void run() {
-
+        // TEST Code
+        boolean clientSession = true;
+        while (clientSession) {
+            String request = network.receive();
+            String response = "";
+            log.info(request);
+            response = "Received: " + request;
+            network.send(response);
+        }
     }
 }
