e8c4838edba817fe64d5b83a1786ee14bfdb60ac
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2025-05-03 21:58:45 +0100

    Add login handler

--- server/src/main/java/ClientHandler.java
+++ server/src/main/java/ClientHandler.java
@@ -29,4 +29,26 @@ public class ClientHandler implements Runnable {
             network.send(response);
         }
     }
+
+    /**
+     * Handle user login
+     *
+     * @param parts Split parts of the request
+     * @return Response to the request.
+     */
+    public String handleLogin(String[] parts) {
+        if (parts.length != 3) return INVALID_REQUEST;
+        if (user != null) return ALREADY_LOGGED_IN;
+        String username = parts[1];
+        String password = parts[2];
+        // TODO: Replace with UserManager.getUser(username) or add UserManager as property of storage
+        User loginUser = storage.users.getUser(username);
+
+        if (loginUser == null) return INVALID_CREDENTIALS;
+        if (!loginUser.getPassword().equals(password)) return INVALID_CREDENTIALS;
+
+        user = loginUser;
+
+        return LOGIN_SUCCESS;
+    }
 }
