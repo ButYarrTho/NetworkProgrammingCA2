34260103c9ff8c7d0d3a0b2279a99cedc1ee4f88
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2025-05-03 19:00:22 +0100

    Implement runnable interface on client handler and add default constructor

--- server/src/main/java/ClientHandler.java
+++ server/src/main/java/ClientHandler.java
@@ -1,2 +1,14 @@
-public class ClientHandler {
+public class ClientHandler implements Runnable {
+    private final TCPNetworkLayer networkLayer;
+    private final Storage storage;
+
+    public ClientHandler(TCPNetworkLayer networkLayer, Storage storage) {
+        this.networkLayer = networkLayer;
+        this.storage = storage;
+    }
+
+    @Override
+    public void run() {
+
+    }
 }
