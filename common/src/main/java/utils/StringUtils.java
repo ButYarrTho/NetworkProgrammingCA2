6765611b8886bae1af14ba47793f265990fee2aa
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2025-05-03 22:08:50 +0100

    Add JavaDoc to isEmail method

--- common/src/main/java/utils/StringUtils.java
+++ common/src/main/java/utils/StringUtils.java
@@ -6,7 +6,14 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringUtils {
+    /**
+     * Checks whether string is an email address.
+     *
+     * @param check The string to be checked.
+     * @return True iff string is an email. False otherwise.
+     */
     public static boolean isEmail(@NonNull String check) {
+        // Email pattern from https://stackoverflow.com/a/201378
         Pattern emailPattern = Pattern.compile("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])");
         Matcher matcher = emailPattern.matcher(check);
         return matcher.find();
