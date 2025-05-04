8fdf36120f38098e8ce13ec0d1633e4c59bf53c8
dylan <dylanhabis@yahoo.com>
2025-05-04 19:15:05 +0100

    Updated Email.java with more fields

--- model/src/main/java/Email.java
+++ model/src/main/java/Email.java
@@ -1,16 +1,25 @@
+package model;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NonNull;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Data
 @AllArgsConstructor
 @Builder
 public class Email {
-    private String subject;
+    private int id;                     // Unique ID
     @NonNull
-    private String body;
-    private LocalDateTime timestamp;
+    private String sender;              // Sender username
+    @NonNull
+    private List<String> recipients;    // Recipients list
+    @NonNull
+    private String subject;             // Subject of email
+    @NonNull
+    private String body;                // Body of email
+    private LocalDateTime timestamp;    // When it was sent
 }
