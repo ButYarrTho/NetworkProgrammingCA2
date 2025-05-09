e4539d24d128ffefdd8e38d0b9cf0bbac3352701
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2025-05-09 17:40:27 +0100

    Add properties to user

--- model/src/main/java/User.java
+++ model/src/main/java/User.java
@@ -1,2 +1,12 @@
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NonNull;
+
+@Data
+@AllArgsConstructor
 public class User {
+    @NonNull
+    private String email;
+    @NonNull
+    private String password;
 }
