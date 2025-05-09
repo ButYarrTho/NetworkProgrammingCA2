e1029e70202997da76066f78be3e846d6345df8b
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2025-05-09 15:34:48 +0100

    Add Network Layer interface

---
+++ network/src/main/java/INetworkLayer.java
@@ -0,0 +1,13 @@
+import java.io.IOException;
+
+public interface INetworkLayer {
+    void connect() throws IOException;
+
+    void send(String message);
+
+    String receive();
+
+    void close();
+
+    void disconnect() throws IOException;
+}
