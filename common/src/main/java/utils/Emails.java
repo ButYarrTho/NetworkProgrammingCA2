f886479ad9791efdb7f1ee043a02748fb35316f9
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2025-05-09 21:26:34 +0100

    Refactor formatting methods - Allow to specify the delimiters

--- common/src/main/java/utils/Emails.java
+++ common/src/main/java/utils/Emails.java
@@ -4,7 +4,7 @@ import model.Email;
 
 import java.time.format.DateTimeFormatter;
 
-import static protocols.EmailUtilities.*;
+import static protocols.EmailUtilities.TIMESTAMP;
 
 public class Emails {
     private static final DateTimeFormatter timestampFormat = DateTimeFormatter.ofPattern(TIMESTAMP);
@@ -12,32 +12,35 @@ public class Emails {
     /**
      * Format the header of an email into a response string
      *
-     * @param email The email to be formatted
+     * @param email     Email to be formatted
+     * @param delimiter Character sequence delimiting the values
      * @return String representation of the email's headers
      */
-    public static String toHeaderResponseString(Email email) {
+    public static String toHeaderResponseString(Email email, String delimiter) {
         return email.getId() +
-                SUBDELIMITER +
+                delimiter +
                 email.getSender() +
-                SUBDELIMITER +
+                delimiter +
                 email.getSubject() +
-                SUBDELIMITER +
+                delimiter +
                 email.getTimestamp().format(timestampFormat);
     }
 
     /**
      * Format the headers of multiple emails into a response string
      *
-     * @param emails The emails to be formatted
+     * @param emails       The emails to be formatted
+     * @param delimiter    Character sequence delimiting the different emails
+     * @param subdelimiter Character sequence delimiting the values
      * @return String representation of the emails headers
      */
-    public static String toHeaderResponseString(Email[] emails) {
+    public static String toHeaderResponseString(Email[] emails, String delimiter, String subdelimiter) {
         StringBuilder formattedEmails = new StringBuilder();
         for (int i = 0; i < emails.length; i++) {
-            formattedEmails.append(toHeaderResponseString(emails[i]));
-            if (i != emails.length - 1) formattedEmails.append(DELIMITER);
+            formattedEmails.append(toHeaderResponseString(emails[i], subdelimiter));
+            if (i != emails.length - 1) formattedEmails.append(delimiter);
         }
 
-        return formattedEmails.substring(0, formattedEmails.length() - DELIMITER.length());
+        return formattedEmails.substring(0, formattedEmails.length() - delimiter.length());
     }
 }
