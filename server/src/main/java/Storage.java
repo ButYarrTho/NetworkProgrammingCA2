70287854aa48607c49e0006eba9432d1c9f22a39
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2025-05-09 18:47:46 +0100

    Add seed method to storage class

--- server/src/main/java/Storage.java
+++ server/src/main/java/Storage.java
@@ -1,6 +1,37 @@
+import java.util.ArrayList;
+
 public class Storage {
     public final UserManager userManager = new UserManager();
     public final EmailManager emailManager = new EmailManager();
 
+    /**
+     * Seed data
+     */
+    public void seed() {
+        if (userManager.size() != 0) {
+            return;
+        }
+
+        User[] users = new User[]{
+                new User("user1@email.com", "password"),
+                new User("user2@email.com", "password"),
+                new User("user3@email.com", "password")
+        };
+
+        for (User user : users) {
+            userManager.addUser(user);
+        }
+
+        ArrayList<String> recipients = new ArrayList<>();
+        recipients.add(users[1].getEmail());
+        recipients.add(users[2].getEmail());
+
+        emailManager.sendEmail(
+                users[0].getEmail(),
+                recipients,
+                "First Email",
+                "This is my first email.\n\nBest regards,\nUser 1"
+        );
+    }
 }
 
